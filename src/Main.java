90f34d2c18763e58204e2aab7f2c203a548fd900
AyeshW <ayeshweerasinghe.16@cse.mrt.ac.lk>
2020-09-15 20:37:20 +0100

    Added multiple argument support for inter-arrival time mean

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        Environment environment = new Environment(50,1);
+        Environment environment;
+        if (args.length != 0) {
+            long busMeanTime = Long.parseLong(args[0]) * 60 * 1000;
+            long riderMeanTime = Long.parseLong(args[1]) * 1000;
+            environment = new Environment(busMeanTime, riderMeanTime);
+        } else {
+            environment = new Environment(20 * 60, 30);
+        }
         try {
             environment.startSimulation(500);
         } catch (InterruptedException e) {
